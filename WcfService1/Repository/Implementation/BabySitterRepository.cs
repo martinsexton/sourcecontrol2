9cd0d64ef82a61b977b9900121a84f548dce813c
Martin Sexton <sexton.martin@gmail.com>
2015-07-14 22:29:20 +0100

    Constructor added to service to receive babysitter repositoru

--- WcfService1/Repository/Implementation/BabySitterRepository.cs
+++ WcfService1/Repository/Implementation/BabySitterRepository.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Repository.API;
 using DomainModel;
+using Repository.API;
 
 namespace Repository.Implementation
 {
--- WcfService1/WcfService1/BabySitterClubService.svc.cs
+++ WcfService1/WcfService1/BabySitterClubService.svc.cs
@@ -5,13 +5,18 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using Repository.API;
 
 namespace WcfService1
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IBabySittersClubService
+    public class BabySitterClubService : IBabySittersClubService
     {
+        private IBabySitterRepository _babySitterRepository;
+
+        public BabySitterClubService(IBabySitterRepository babySitterRepository)
+        {
+            _babySitterRepository = babySitterRepository;
+        }
 
         public BabySitter GetBabySittersByEircode(string eircode)
         {
