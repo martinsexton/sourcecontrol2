6a9d5ba3d5570931d59574590c430c50e37e4062
Martin Sexton <sexton.martin@gmail.com>
2015-07-14 22:21:10 +0100

    Initial Checkin

---
+++ WcfService1/Repository/API/IBabySitterRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainModel;
+
+
+
+namespace Repository.API
+{
+    interface IBabySitterRepository
+    {
+        BabySitter getBabySitter(String firstname, String surname);
+    }
+}
---
+++ WcfService1/Repository/Implementation/BabySitterRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.API;
+using DomainModel;
+
+namespace Repository.Implementation
+{
+    class BabySitterRepository : IBabySitterRepository
+    {
+
+        public BabySitter getBabySitter(string firstname, string surname)
+        {
+            return new BabySitter();
+        }
+    }
+}
