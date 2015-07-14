a51bdc2bad132f3a6bf196aef8401fd01d1000e2
Martin Sexton <sexton.martin@gmail.com>
2015-07-14 22:21:47 +0100

    Add untracked files

---
+++ WcfService1/DomainModel/BabySitter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class BabySitter
+    {
+        private String firstName;
+        private String surname;
+
+        public String Firstname
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public String Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+    }
+}
