fe7f923b41fec703a8a73b34f118f55f4ea963c1
Martin Sexton <sexton.martin@gmail.com>
2016-03-08 15:15:14 +0000

    Multi Tenant

--- SecuredBOMIApplication/SecuredBOMIApplication/App_Start/Startup.Auth.cs
+++ SecuredBOMIApplication/SecuredBOMIApplication/App_Start/Startup.Auth.cs
@@ -2,6 +2,7 @@
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OpenIdConnect;
+using System.Collections.Generic;
 
 namespace SecuredBOMIApplication
 {
@@ -9,14 +10,27 @@ namespace SecuredBOMIApplication
     {
         public void ConfigureAuth(IAppBuilder app)
         {
+            List<string> validTenants = new List<string>();
+
+            //Add BOMI as a trusted Tenant
+            validTenants.Add("https://sts.windows.net/65d15693-ed84-471e-88e6-b21c16a2af42/");
+            //Add HSE as a trusted Tenant
+            validTenants.Add("https://sts.windows.net/8573f58c-1c22-4de9-9935-dbfec34ce5f3/");
+
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
             app.UseOpenIdConnectAuthentication(
                 new OpenIdConnectAuthenticationOptions
                 {
                     ClientId = "33abe5d0-db4e-4371-9dab-d8ddfdcb5ec7",
-                    Authority = "https://login.windows.net/sextonmartingmail.onmicrosoft.com",
-                    PostLogoutRedirectUri = "https://localhost:44300/"
+                    //Authority = "https://login.windows.net/sextonmartingmail.onmicrosoft.com",
+                    Authority = "https://login.microsoftonline.com/common",
+                    PostLogoutRedirectUri = "https://localhost:44300/",
+                    TokenValidationParameters = new System.IdentityModel.Tokens.TokenValidationParameters
+                    {
+                        //ValidateIssuer = false
+                        ValidIssuers = validTenants
+                    }
                 }
             );
         }
