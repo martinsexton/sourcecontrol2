cedf24f365b768445dcf690c44cfeed7b3a2feb0
Martin Sexton <sexton.martin@gmail.com>
2017-03-08 00:08:54 +0000

    updates to controller

--- SecuredApp/SecuredApp/Scripts/projects.js
+++ SecuredApp/SecuredApp/Scripts/projects.js
@@ -1,26 +1,5 @@
 ﻿var postApp = angular.module('postApp', []);
-postApp.controller('postController', function ($scope, $http) {
-    // create a blank object to handle form data.
+postApp.controller('postController', ['$scope', function ($scope) {
+    //Create empty project object
     $scope.project = {};
-    // calling our submit function.
-    $scope.submitForm = function () {
-        alert('sdfsdf');
-        // Posting data to php file
-        /*$http({
-            method: 'POST',
-            url: 'clone.php',
-            data: $scope.project, //forms user object
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        })
-          .success(function (data) {
-              if (data.errors) {
-                  // Showing errors.
-                  $scope.errorName = data.errors.name;
-                  $scope.errorUserName = data.errors.username;
-                  $scope.errorEmail = data.errors.email;
-              } else {
-                  $scope.message = data.message;
-              }
-          });*/
-    };
-});
+}]);
