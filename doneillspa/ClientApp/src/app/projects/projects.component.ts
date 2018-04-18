5fe28fae3ccf59d2e933b24d55785e4c6a93131f
sexton.martin <sexton.martin@gmail.com>
2018-04-19 00:05:05 +0100

    Improvements to UI

--- doneillspa/ClientApp/src/app/projects/projects.component.ts
+++ doneillspa/ClientApp/src/app/projects/projects.component.ts
@@ -15,11 +15,31 @@ export class ProjectComponent {
   public projects: Project[];
   newProject: Project = new Project('', '', true);
   projectSaved: boolean = false;
+  displayAddProject = false;
+  selectedProject: Project = null;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private _projectService: ProjectService) {
     this._projectService.getProjects().subscribe(result => {
       this.projects = result;
     }, error => console.error(error));
+  }
+
+  testMethod(project) {
+    console.log("testMethod Clicked: " + project.name);
+    this.selectedProject = project;
+  }
+
+  toggleDisplayAddProjectForm() {
+    this.displayAddProject = !this.displayAddProject;
+  }
+
+  getProjectButtonStyle() {
+    if (this.displayAddProject) {
+      return "glyphicon glyphicon-minus";
+    }
+    else {
+      return "glyphicon glyphicon-plus";
+    }
   }
 
   saveProject() {
@@ -31,6 +51,7 @@ export class ProjectComponent {
         this.projects.push(new Project(this.newProject.name, this.newProject.details, this.newProject.isactive));
         //clear down the new project model
         this.newProject = new Project('', '', true);
+        this.displayAddProject = false;
       },
       (err: HttpErrorResponse) => {
         console.log(err.error);
