ddbb45706eafec9726ec856c27069d5904896b48
sexton.martin <sexton.martin@gmail.com>
2018-04-20 08:09:32 +0100

    hide registration box from home page when logged in

--- doneillspa/ClientApp/src/app/home/home.component.ts
+++ doneillspa/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MsUserService } from '../shared/services/msuser.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  constructor(private userService: MsUserService) { }
+
+  isUserLoggedIn() {
+    return this.userService.isLoggedIn();
+  }
 }
