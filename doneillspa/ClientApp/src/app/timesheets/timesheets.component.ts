462d299b857a55faf4c21351baa25cdff736738b
sexton.martin <sexton.martin@gmail.com>
2018-04-28 00:17:32 +0100

    tabes for days of week added

--- doneillspa/ClientApp/src/app/timesheets/timesheets.component.ts
+++ doneillspa/ClientApp/src/app/timesheets/timesheets.component.ts
@@ -15,10 +15,48 @@ declare var $: any;
 
 export class TimesheetComponent {
   public timesheets: Timesheet[];
+
+  currentDate: Date;
+
+  monday: DayOfWeek;
+  tues: DayOfWeek;
+  wed: DayOfWeek;
+  thurs: DayOfWeek;
+  fri: DayOfWeek;
+  sat: DayOfWeek;
+
   displayAddTimesheet = false;
   newTimesheet: Timesheet = new Timesheet(localStorage.getItem('client_id'), new Date());
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private _projectService: ProjectService) {
+    this.currentDate = new Date();
+
+    let day = this.currentDate.getDay();
+
+    var monday = new Date();
+    monday.setDate(monday.getDate() - (day - 1))
+    this.monday = new DayOfWeek("Mon", monday);
+
+    var tuesday = new Date();
+    tuesday.setDate(monday.getDate() + 1);
+    this.tues = new DayOfWeek("Tue", tuesday);
+
+    var wednesday = new Date();
+    wednesday.setDate(tuesday.getDate() + 1);
+    this.wed = new DayOfWeek("Wed", wednesday);
+
+    var thursday = new Date();
+    thursday.setDate(wednesday.getDate() + 1);
+    this.thurs = new DayOfWeek("Thurs", thursday);
+
+    var friday = new Date();
+    friday.setDate(thursday.getDate() + 1);
+    this.fri = new DayOfWeek("Fri", friday);
+
+    var saturday = new Date();
+    saturday.setDate(friday.getDate() + 1);
+    this.sat = new DayOfWeek("Sat", saturday);
+
     this._projectService.getTimesheets().subscribe(result => {
       this.timesheets = result;
     }, error => console.error(error));
@@ -49,3 +87,10 @@ export class TimesheetComponent {
     );
   }
 }
+
+export class DayOfWeek {
+  constructor(
+    public nameOfDay: string,
+    public date: Date
+  ) { }
+} 
