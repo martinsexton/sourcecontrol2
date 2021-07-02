72f20cd27cf5a7780916af9d1676d7591aaeb806
sexton.martin <sexton.martin@gmail.com>
2021-07-02 12:15:41 +0100

    test checkin

--- doneillspa/Controllers/AuthController.cs
+++ doneillspa/Controllers/AuthController.cs
@@ -33,7 +33,7 @@ namespace doneillspa.Controllers
         {
             JsonResult result;
             var expiresIn = TimeSpan.FromDays(7);
-            //var expiresIn = TimeSpan.FromSeconds(10);  
+            //var expiresIn = TimeSpan.FromSeconds(10);
 
             var identity = await GetClaimsIdentity(credentials.FirstName+credentials.Surname.Replace(" ", string.Empty), credentials.Password);
             if (identity == null)
