160460effdbace4b95521cfbd9d91273bf1db21d
sexton.martin <sexton.martin@gmail.com>
2021-08-05 22:09:42 +0100

    included a new payable field on timesheet details.

--- doneillspa/ClientApp/src/app/components/timesheetentrylist/timesheetentrylist.component.ts
+++ doneillspa/ClientApp/src/app/components/timesheetentrylist/timesheetentrylist.component.ts
@@ -13,7 +13,87 @@ import { TimesheetEntry } from '../../timesheetentry';
 export class TimeSheetEntryListComponent {
   @Input() owner: Timesheet;
 
-  constructor(private _timesheetService: TimesheetService) {}
+  constructor(private _timesheetService: TimesheetService) { }
+
+  calculateTotalPayableDuration() {
+    let totalDuration: number = 0;
+
+    let ts = this.owner;
+
+    //We will need to separate timesheets into the differnt days and add
+    //totals for each day and if >= 5 hours substract 30 minutes for lunch breaks
+    let mondayMins: number = 0;
+    let tueMins: number = 0;
+    let wedMins: number = 0;
+    let thursMins: number = 0;
+    let friMins: number = 0;
+    let satMins: number = 0;
+    let sunMins: number = 0;
+
+    for (let tse of ts.timesheetEntries) {
+      //Exclude time recorded against Sick Days
+      if (tse.code == 'NC4') {
+        continue;
+      }
+      let day = tse.day;
+      var start = new Date("2018-01-01 " + tse.startTime);
+      var end = new Date("2018-01-01 " + tse.endTime);
+
+      var elapsedTimeInSec = (end.getTime() - start.getTime()) / 1000;
+      var elapsedTimeInMins = elapsedTimeInSec / 60;
+
+      if (day == "Mon") {
+        mondayMins += elapsedTimeInMins;
+      }
+      else if (day == "Tue") {
+        tueMins += elapsedTimeInMins;
+      }
+      else if (day == "Wed") {
+        wedMins += elapsedTimeInMins;
+      }
+      else if (day == "Thurs") {
+        thursMins += elapsedTimeInMins;
+      }
+      else if (day == "Fri") {
+        friMins += elapsedTimeInMins;
+      }
+      else if (day == "Sat") {
+        satMins += elapsedTimeInMins;
+      }
+      else {
+        sunMins += elapsedTimeInMins;
+      }
+    }
+
+    //If worked >= 5 hours for a day subtract 30 mins.
+    if (mondayMins >= (5 * 60)) {
+      mondayMins = mondayMins - 30;
+    }
+    if (tueMins >= (5 * 60)) {
+      tueMins = tueMins - 30;
+    }
+    if (wedMins >= (5 * 60)) {
+      wedMins = wedMins - 30;
+    }
+    if (thursMins >= (5 * 60)) {
+      thursMins = thursMins - 30;
+    }
+    if (friMins >= (5 * 60)) {
+      friMins = friMins - 30;
+    }
+    if (satMins > (5 * 60)) {
+      satMins = satMins - 30;
+    }
+    if (sunMins > (5 * 60)) {
+      sunMins = sunMins - 30;
+    }
+    totalDuration = mondayMins + tueMins + wedMins + thursMins + friMins + satMins + sunMins;
+
+    var hours = Math.floor(totalDuration / 60);
+    var minutes = totalDuration % 60;
+
+    return hours + ':' + minutes;
+  }
 
   calculateTotalDuration(): string {
     let totalDuration: number = 0;
