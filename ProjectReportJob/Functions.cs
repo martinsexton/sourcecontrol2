42837a3feff5ee86096078f09ee12b4088a45b70
sexton.martin <sexton.martin@gmail.com>
2022-04-11 16:35:11 +0100

    added webjob project

---
+++ ProjectReportJob/Functions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace ProjectReportJob
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("messages")] GenerateReportEvent message, TextWriter log)
+        {
+            log.WriteLine(message.ProjectCode);
+        }
+    }
+}
