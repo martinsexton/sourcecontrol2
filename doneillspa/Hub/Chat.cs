21744d893b38e6fba731553ded350fc283f40694
sexton.martin <sexton.martin@gmail.com>
2022-04-20 15:51:42 +0100

    added method onto signalr hub

--- doneillspa/Hub/Chat.cs
+++ doneillspa/Hub/Chat.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
 
 namespace hub
 {
     public class Chat : Hub
     {
+        public void SendMessage(string msg)
+        {
+           Clients.All.SendAsync("timesheetsubmitted", msg);
+        }
     }
 }
