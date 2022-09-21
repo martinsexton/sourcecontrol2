6a236360af7b64017ce4dc72a01e46c474855ec5
sexton.martin <sexton.martin@gmail.com>
2022-09-21 22:22:59 +0100

    fix

--- doneillspa/Startup.cs
+++ doneillspa/Startup.cs
@@ -129,7 +129,7 @@ namespace doneillspa
            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // This method gets called by the runtime. Use this method to configure the HTTP  request pipeline.
         public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
